04c615d4638df28ab8b67838f487787131e40abe
Antonikas58 <anton.liolia@leverx.com>
2019-12-30 14:00:36 +0300

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -9,17 +10,33 @@ import { CartService } from '../cart.service';
 export class CartComponent implements OnInit {
 
   items;
+  checkoutForm;
   constructor(
-  private cartService: CartService
+  private cartService: CartService,
+  private formBuilder: FormBuilder
+  ) { 
+    this.items = this.cartService.getItems();
 
-  ) { }
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+  }
 
  ngOnInit() {
-    this.items = this.cartService.getItems();
+
   }
  ClearCart(){
    this.cartService.clearCart();
    this.items = this.cartService.getItems();
    window.alert('Your products has been deleted from the cart!');
  }
+onSubmit(customerData) {
+    // Process checkout data here
+    console.warn('Your order has been submitted', customerData);
+
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+  }
+
 }
